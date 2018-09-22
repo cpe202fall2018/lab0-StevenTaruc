4f802fef3865f10f6436f0c86edd6085df73e655
Steven Taruc <sjrt18@gmail.com>
2018-09-21 19:03:06 -0700

    final

--- planets.py
+++ planets.py
@@ -13,7 +13,7 @@ def weight_on_planets():
     # print("What do you weigh on earth? {}\n\nOn Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds.".format(weight, weight*0.38, weight*2.34))
     print("What do you weigh on earth? \nOn Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds.".format(weight*0.38, weight*2.34))
     # write your code here
-    # Passes Unit test. The commented out print statement matches the lab specs from polylearn
+    # Passes planet_tests.py Unit test. The commented out print statement matches the lab specifications from polylearn
 
    
    
