620ef42a3da18f9a1dc3369b2dad51a5e8c349ba
Steven Taruc <sjrt18@gmail.com>
2018-09-24 16:50:25 -0700

    Added purpose statement

--- planets.py
+++ planets.py
@@ -6,8 +6,9 @@
 # Lab 0
 # Section 13/14
 # Purpose of Lab: Environment, Python, and Testing!
-# Additional Comments
+# https://polylearn.calpoly.edu/AY_2018-2019/pluginfile.php/243868/mod_resource/content/0/lab00.pdf
 
+# Takes integer earth weight as input from input() and converts it to Mars and Jupiter weight
 def weight_on_planets():
     weight = int(input("What do you weigh on earth? "))
     print("\nOn Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds.".format(weight*0.38, weight*2.34))
